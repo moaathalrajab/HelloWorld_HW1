c6ea1bf07d8770ed855cb9d02db01db682fe5fa0
Moaath Alrajab <moaath@alrajab.net>
2021-09-09 20:34:49 -0400

    Adding Btn

--- app/src/main/java/com/leuenroo/helloworld_hw1/MainActivity.java
+++ app/src/main/java/com/leuenroo/helloworld_hw1/MainActivity.java
@@ -2,13 +2,30 @@ package com.leuenroo.helloworld_hw1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public void moveToUI2(View view) {
+        startActivity(new Intent(this, MainActivity2.class));
     }
 }
